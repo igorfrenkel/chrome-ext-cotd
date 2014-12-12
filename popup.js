b9eb6e958edca15cfe485202f62ce87762bf01af
Igor Frenkel <igor.frenkel@lookout.com>
2014-12-11 22:29:40 -0500

    Add tab selection for different galleries

--- popup.js
+++ popup.js
@@ -1,7 +1,7 @@
 var _idMatcher = /[^\/]+/g;
 var _gallery;
 var _thumbUrl = function(url) {
-  var id = url.match(_idMatcher).pop(),
+  var id = url.match(_idMatcher).pop().replace(".jpg",""),
     thumbUrl = "http://imgur.com/" + id + "s.jpg";
   return thumbUrl;
 };
@@ -11,77 +11,132 @@ var gallery = function() {
   return _gallery;
 };
 
-var addBookmarkThumbToContainer = function(bookmark, container) {
-  var imgUrl = bookmark.url,
-      thumbUrl = _thumbUrl(imgUrl),
-      link = document.createElement("a"),
-      thumb = document.createElement("img");
-  link.href = imgUrl;
-  link.target = "_blank";
-  thumb.src = thumbUrl;
-  link.appendChild(thumb);
-  container.appendChild(link);
-};
+var GalleryFolder = function(tabName, folderSubtree) {
+  var _galleryFolderContainer;
 
-var _makeLink = function(inner, href, cb) {
-  var link = document.createElement("a");
-  link.href = href;
-  link.innerText = inner;
-  link.onclick = cb;
-  return link;
-};
+  var addBookmarkThumbToContainer = function(bookmark, container) {
+    var imgUrl = bookmark.url,
+        thumbUrl = _thumbUrl(imgUrl),
+        link = document.createElement("a"),
+        thumb = document.createElement("img");
+    link.href = imgUrl;
+    link.target = "_blank";
+    thumb.src = thumbUrl;
+    link.appendChild(thumb);
+    container.appendChild(link);
+  };
 
-var addBookmarkParentToContainer = function(bookmark, container) {
-  if (bookmark.parentId) {
-    chrome.bookmarks.get(bookmark.parentId, function(results) {
-      var title = document.createElement("h6");
-      title.innerText = results[0].title;
-      container.appendChild(title);
-      var moveToPending = _makeLink("\u2717", "#", function(e) {
-        chrome.bookmarks.move(bookmark.id, { parentId: pending().id }, function() {
-          title.innerText = "Pending";
+  var _makeLink = function(inner, href, cb) {
+    var link = document.createElement("a");
+    link.href = href;
+    link.innerText = inner;
+    link.onclick = cb;
+    link.target = "_blank";
+    return link;
+  };
+
+  var addBookmarkParentToContainer = function(bookmark, container) {
+    if (bookmark.parentId) {
+      chrome.bookmarks.get(bookmark.parentId, function(results) {
+        var title = document.createElement("h6");
+        title.innerText = results[0].title;
+        container.appendChild(title);
+        var moveToPending = _makeLink("\u2717", "#", function(e) {
+          e.preventDefault();
+          chrome.bookmarks.move(bookmark.id, { parentId: pending().id }, function() {
+            title.innerText = "Pending";
+          });
         });
-      });
-      var moveToSubmitted = _makeLink("\u2713", "#", function(e) {
-        chrome.bookmarks.move(bookmark.id, { parentId: submitted().id }, function() {
-          title.innerText = "Submitted";
+        var moveToSubmitted = _makeLink("\u2713", "#", function(e) {
+          e.preventDefault();
+          chrome.bookmarks.move(bookmark.id, { parentId: submitted().id }, function() {
+            title.innerText = "Submitted";
+          });
         });
+        container.appendChild(moveToPending);
+        container.appendChild(moveToSubmitted);
       });
-      container.appendChild(moveToPending);
-      container.appendChild(moveToSubmitted);
+    }
+  };
+
+  var addBookmarkToPopup = function(bookmark) {
+    var container = document.createElement("div"),
+        thumbPart = document.createElement("div"),
+        parentPart = document.createElement("div");
+
+    container.className = "thumb-container";
+    container.appendChild(thumbPart);
+    container.appendChild(parentPart);
+    _galleryFolderContainer.appendChild(container);
+    addBookmarkThumbToContainer(bookmark, thumbPart);
+    addBookmarkParentToContainer(bookmark, parentPart);
+  };
+
+  var drawGalleryFrom = function(bookmarks) {
+    bookmarks.forEach(function(bookmark) {
+      addBookmarkToPopup(bookmark);
     });
-  }
-};
+  };
 
-var addBookmarkToPopup = function(bookmark) {
-  var container = document.createElement("div"),
-      thumbPart = document.createElement("div"),
-      parentPart = document.createElement("div");
-
-  container.className = "thumb-container";
-  container.appendChild(thumbPart);
-  container.appendChild(parentPart);
-  gallery().appendChild(container);
-  addBookmarkThumbToContainer(bookmark, thumbPart);
-  addBookmarkParentToContainer(bookmark, parentPart);
-};
-
-var drawGalleryFrom = function(bookmarks) {
-  bookmarks.forEach(function(bookmark) {
-    console.log(bookmark);
-    addBookmarkToPopup(bookmark);
-  });
-};
-
-var renderFolder = function(folder) {
-  chrome.bookmarks.getSubTree(folder.id, function(results) {
-    var bookmarks = [];
-    results[0].children.forEach(function(result) {
-      if (result.url)
-        bookmarks.push(result);
+  var renderThumbs = function(folder) {
+    chrome.bookmarks.getSubTree(folder.id, function(results) {
+      var bookmarks = [];
+      results[0].children.forEach(function(result) {
+        if (result.url)
+          bookmarks.push(result);
+      });
+      drawGalleryFrom(bookmarks);
     });
-    drawGalleryFrom(bookmarks);
-  });
+  };
+
+  var enable = function(element) {
+    element.className = "tab active";
+  };
+
+  var disable = function(element) {
+    element.className = "tab";
+  };
+
+  this.enable = function() {
+    enable(_galleryFolderContainer);
+  };
+
+  this.disable = function() {
+    disable(_galleryFolderContainer);
+  };
+
+  var makeTabCalled = function(tabName) {
+    var header = document.createElement("h3"),
+      a = document.createElement("a"),
+      currentTab = _galleryFolderContainer;
+    a.innerText = tabName;
+    a.href = "#";
+    a.onclick = function(e) {
+      e.preventDefault();
+      var elements = document.getElementsByClassName("tab"),
+        element, i;
+      for(i =0; i < elements.length; i++) {
+        element = elements[i];
+        disable(element);
+        if (element != currentTab) 
+          enable(element);
+      }
+    };
+    header.appendChild(a);
+    _galleryFolderContainer.appendChild(header);
+  };
+
+  var initWith = function(parent) {
+    _galleryFolderContainer = document.createElement("div");
+    _galleryFolderContainer.className = "tab";
+    parent.appendChild(_galleryFolderContainer);
+  };
+
+  this.renderOn = function(parentContainer) {
+    initWith(parentContainer);
+    makeTabCalled(tabName);
+    renderThumbs(folderSubtree);
+  };
 };
 
 var _pending, _submitted;
@@ -97,11 +152,14 @@ document.addEventListener('DOMContentLoaded', function () {
       results[0].children.forEach(function(item) {
         if (item.title == "Pending") {
           _pending = item; 
-          renderFolder(item);
+          var folder = new GalleryFolder("Pending", item)
+          folder.renderOn(gallery());
+          folder.enable();
         }
         else if (item.title == "Submitted") {
           _submitted = item; 
-          renderFolder(item);
+          var folder = new GalleryFolder("Submitted", item)
+          folder.renderOn(gallery());
         }
       });
     });
